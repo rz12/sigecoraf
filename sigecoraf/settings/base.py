dcdb0f6b96df49c384efa98c49130f652a3e07ca
Jorge Luis Malla Sánchez <jomasloja@gmail.com>
2018-05-03 00:47:28 -0500

    add static

--- sigecoraf/settings/base.py
+++ sigecoraf/settings/base.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = "staticfiles"
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
